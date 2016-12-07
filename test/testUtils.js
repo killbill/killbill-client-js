3deaccedb63925320402aa33955df8a57c98d925
Otávio Augusto Soares <otaviokk@gmail.com>
2016-12-07 02:05:46 -0200

    make node compatible

--- test/testUtils.js
+++ test/testUtils.js
@@ -5,9 +5,7 @@ var chai = require('chai');
 expect = chai.expect;
 assert = chai.assert;
 
-dateFormat = require('dateformat');
-
-require('../lib/killbill');
-require('../lib/resources/Account');
-require('../lib/resources/Subscription');
-require('../lib/resources/Security');
+// require('../lib/killbill');
+// require('../lib/resources/Account');
+// require('../lib/resources/Subscription');
+// require('../lib/resources/Security');
